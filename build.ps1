0a465d69d103781416468d35c29ca00cf9a6fe36
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-11 00:16:05 -0700

    Added 2.0.1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 ﻿#Remove-Item –path ./ –recurse
-& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/barcode -l php -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/barcode -l php -c packageconfig.json
 #(Get-Content ./client/package.json).replace('v1', '1.0.1') | Set-Content ./client/package.json
 Copy-Item ./cloudmersive_barcode_api_client/* -Destination . -Recurse -Force
 Remove-Item –path ./cloudmersive_barcode_api_client –recurse
